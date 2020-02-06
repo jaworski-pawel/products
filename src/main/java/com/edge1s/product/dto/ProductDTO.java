c95b2acdef5e23a0a8e02c6a99dbd48c8bf46a08
Pawel Jaworski <pawel.jaworski@live.com>
2020-02-06 15:16:43 +0100

    calculation of the discounted price

--- src/main/java/com/edge1s/product/dto/ProductDTO.java
+++ src/main/java/com/edge1s/product/dto/ProductDTO.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -26,11 +27,18 @@ public class ProductDTO {
         this.name = product.getName();
         this.description = product.getDescription();
         this.type = product.getType().getName();
-        this.price = product.getPrice();
+        this.price = calculateTheDiscountPrice(product);
         this.views = product.getViews();
     }
 
     public void increaseViews() {
         views++;
+    }
+
+    private BigDecimal calculateTheDiscountPrice(Product product) {
+        return product
+                .getPrice()
+                .multiply(BigDecimal.valueOf(100 - product.getType().getDiscountInPercent()))
+                .divide(BigDecimal.valueOf(100), 2, RoundingMode.UP);
     }
 }
